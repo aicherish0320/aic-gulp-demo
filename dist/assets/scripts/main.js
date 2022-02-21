bf4d633e5ee02c3faee7aa34684b5449a21fd333
aicherish0320 <aicherish0320@163.com>
2022-02-21 17:18:21 +0800

    feat: 自动化构建过程重构

--- dist/assets/scripts/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var appendChild = function appendChild(tagName) {
-  var tag = document.createElement(tagName);
-  tag.innerHTML = '<h1>Hello Gulp</h1>';
-  document.body.appendChild(tag);
-};
-
-appendChild();
